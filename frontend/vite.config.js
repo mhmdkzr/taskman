86845b9f37ff6f5374d3822e7fde8720bd247c22
Mohammad Kazar <mhmdkazar@gmail.com>
2026-07-27 16:12:00 +0330

    feat: integrate Tailwind CSS v4, shadcn-svelte, and icon libraries

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [tailwindcss(), svelte()],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+    },
+  },
 })
